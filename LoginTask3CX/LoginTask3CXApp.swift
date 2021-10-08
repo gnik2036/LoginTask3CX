21fed896c113a5b46f40862fc0345c69824cb78e
mohammed hassan <gnik203@gmail.com>
2021-10-08 09:40:00 +0200

    init

---
+++ LoginTask3CX/LoginTask3CXApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginTask3CXApp.swift
+//  LoginTask3CX
+//
+//  Created by Mohammed Hassan on 08/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct LoginTask3CXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
